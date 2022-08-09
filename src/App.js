a547a38f4656db533d98e23e86b9a999d4eb5dd0
0xlvl3 <cryptochoccy@gmail.com>
2022-08-09 20:57:03 +1000

    Progress

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
 
+const welcome = {
+  title: "React",
+  greeting: "Hey",
+};
+
+const num = 10;
+const numArr = [1, 2, 3, 4];
+const float = 1.1;
+
+/**
+ *
+ * @param {string} title
+ * @returns title string passed in will be returned
+ */
+function getTitle(title) {
+  return title;
+}
+
+const exponentialNumbers = numArr.map(function (number) {
+  return number * number;
+});
+console.log(exponentialNumbers);
+
+/**
+ *
+ * @returns renders to the browser
+ */
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>
+        {welcome.greeting} {getTitle("React")} {float} {num} {numArr}
+      </h1>
+      <label htmlFor="search"> Search: </label>
+      <input id="search" type="text" />
     </div>
   );
 }
