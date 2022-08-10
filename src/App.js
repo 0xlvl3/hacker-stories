0dcf790e07d5a3276094451c66baed7c9aad800d
0xlvl3 <cryptochoccy@gmail.com>
2022-08-10 16:30:07 +1000

    Progres

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import * as React from "react";
 
-const welcome = {
-  title: "React",
-  greeting: "Hey",
-};
-
-const num = 10;
+const list = [
+  {
+    title: "React",
+    url: "https://reactjs.org/",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: "https://redux.js.org/",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 4,
+    objectID: 0,
+  },
+];
 const numArr = [1, 2, 3, 4];
-const float = 1.1;
-
-/**
- *
- * @param {string} title
- * @returns title string passed in will be returned
- */
-function getTitle(title) {
-  return title;
-}
-
 const exponentialNumbers = numArr.map(function (number) {
   return number * number;
 });
@@ -30,11 +31,28 @@ console.log(exponentialNumbers);
 function App() {
   return (
     <div>
-      <h1>
-        {welcome.greeting} {getTitle("React")} {float} {num} {numArr}
-      </h1>
+      <h1>My Hacker Stories</h1>
       <label htmlFor="search"> Search: </label>
       <input id="search" type="text" />
+
+      <hr />
+      <ul>
+        {list.map(function (item) {
+          return (
+            <li key={item.objectID}>
+              <span>
+                <a href={item.url}>{item.title}</a>
+              </span>
+              <br />
+              <span>{item.author}</span>
+              <br />
+              <span>{item.num_comments}</span>
+              <br />
+              <span>{item.points}</span>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
