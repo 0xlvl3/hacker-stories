f023231f397dab2c1d016aa3e3258be8be01f49d
ChocBloc <85860569+ChocBloc@users.noreply.github.com>
2022-08-13 11:02:51 +1000

    Progress

--- src/App.js
+++ src/App.js
@@ -28,6 +28,8 @@ const App = () => {
     },
   ];
 
+  console.log(`App renders`);
+
   return (
     <div>
       <h1>My Hacker Stories</h1>
@@ -36,6 +38,7 @@ const App = () => {
 
       <hr />
 
+      <Test words={"This is a test"} myColor="red" />
       <List list={stories} />
     </div>
   );
@@ -43,29 +46,30 @@ const App = () => {
 // We instantiate our components like <Item />
 
 /**
- * React component
+ * React component using React State
  * @returns Renders a search bar
  */
 const Search = () => {
+  console.log("Search renders");
+  const [searchTerm, setSerachTerm] = React.useState("");
+
   //handler for an event
   const handleChange = (event) => {
     //synthetic event
     console.log(event);
 
-    //value of target (here: element)
-    console.log(event.target.value);
+    //value of target (here: element), using setSearchTerm() with useState we can now see our typed input below the search box.
+    setSerachTerm(event.target.value);
   };
 
   //onChange here is our handleChange event
   return (
     <div>
       <label htmlFor="search"> Search: </label>
-      <input
-        id="search"
-        type="text"
-        onChange={handleChange}
-        onBlur={handleChange}
-      />
+      <input id="search" type="text" onChange={handleChange} />
+      <p>
+        Searching for <strong>{searchTerm}</strong>
+      </p>
     </div>
   );
 };
@@ -75,22 +79,55 @@ const Search = () => {
  * @returns Will render the items out of our list object
  */
 const List = (props) => (
-  <ul>
-    {props.list.map((item) => (
-      <Item key={item.objectID} item={item} />
-    ))}
-  </ul>
+  console.log("List renders"),
+  (
+    <ul>
+      {props.list.map((item) => (
+        <Item key={item.objectID} item={item} />
+      ))}
+    </ul>
+  )
 );
 
 const Item = (props) => (
-  <li>
-    <span>
-      <a href={props.item.url}>{props.item.title}</a>
-    </span>
-    <span>{props.item.author}</span>
-    <span>{props.item.num_comments}</span>
-    <span>{props.item.points}</span>
-  </li>
+  console.log("Item renders"),
+  (
+    <li>
+      <span>
+        <a href={props.item.url}>{props.item.title}</a>
+      </span>
+      <span>{props.item.author}</span>
+      <span>{props.item.num_comments}</span>
+      <span>{props.item.points}</span>
+    </li>
+  )
 );
+
+/*
+//PROPS
+///
+
+// using a props to display words
+const Test = (props) => {
+  console.log(`Test rendered`);
+  return <p>{props.words}</p>;
+};
+
+//assigning words object to props and then returning words itself as it is declared
+const Test = (props) => {
+  const { words } = props;
+  return <p>{words}</p>;
+};
+
+//using destructing to assign a prop
+const Test = ({ words }) => {
+  return <p>{words}</p>;
+};
+*/
+
+//destructing multiple props within one
+const Test = ({ words, myColor }) => {
+  return <p style={{ color: myColor }}>{words}</p>;
+};
 
 export default App;
